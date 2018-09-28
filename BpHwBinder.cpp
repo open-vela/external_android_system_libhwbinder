31918057bdfd981e00359d6101dfe0650b8825ca
openvela-robot <openvela-robot@xiaomi.com>
2018-09-28 16:33:04 +0800

    BpHwRefBase: expose 'remote' field. am: 81dbdeedba am: 830a5d059f am: 9531aa50aa
    Change-Id: Icbcacf3888c02cd1bf7fc78dc65ea22fbcb7c7d7

--- BpHwBinder.cpp
+++ BpHwBinder.cpp
@@ -215,13 +215,6 @@ void BpHwBinder::sendObituary()
     }
 }
 
-// Returns the strong refcount on the object this proxy points to, or
-// -1 in case of failure.
-ssize_t BpHwBinder::getNodeStrongRefCount()
-{
-    return ProcessState::self()->getStrongRefCountForNodeByHandle(mHandle);
-}
-
 void BpHwBinder::reportOneDeath(const Obituary& obit)
 {
     sp<DeathRecipient> recipient = obit.recipient.promote();
