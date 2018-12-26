0803c167a023b9da729039843976aea4f1fb5861
openvela-robot <openvela-robot@xiaomi.com>
2018-12-26 13:59:23 +0800

    getCallingSid: get calling security context
    This call gets the security context of the calling process when
    requested.
    
    Bug: 121035042
    Test: boot
    Change-Id: Ia8a4c0cb4a9c86dcc0d3b7583014237f879a3074
    Merged-In: Ia8a4c0cb4a9c86dcc0d3b7583014237f879a3074

--- include/hwbinder/Debug.h
+++ include/hwbinder/Debug.h
@@ -18,14 +18,15 @@
 #define ANDROID_HARDWARE_BINDER_DEBUG_H
 
 #include <stdint.h>
-#include <sys/cdefs.h>
 #include <sys/types.h>
 
 namespace android {
 namespace hardware {
 // ---------------------------------------------------------------------------
 
-__BEGIN_DECLS
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 const char* stringForIndent(int32_t indentLevel);
 
@@ -39,9 +40,9 @@ void printHexData(int32_t indent, const void *buf, size_t length,
     size_t alignment=0, bool cArrayStyle=false,
     debugPrintFunc func = 0, void* cookie = 0);
 
-ssize_t getHWBinderKernelReferences(size_t count, uintptr_t* buf);
-
-__END_DECLS
+#ifdef __cplusplus
+}
+#endif
 
 // ---------------------------------------------------------------------------
 }; // namespace hardware
