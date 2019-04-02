7284c3b48d4b20eeb61e93795c32a03b870fa399
liujinye <liujinye@xiaomi.com>
2019-04-02 13:16:54 +0800

    Remove unused type: small_flat_data am: 9f6d39dc32
    Change-Id: Ieeaf87d7a7dd780dd161294761e368c62dfe7855

--- Binder.cpp
+++ Binder.cpp
@@ -102,7 +102,7 @@ void BHwBinder::setRequestingSid(bool requestingSid) {
         if (!e) return; // out of memory
     }
 
-    e->mRequestingSid = requestingSid;
+    e->mRequestingSid = true;
 }
 
 status_t BHwBinder::transact(
