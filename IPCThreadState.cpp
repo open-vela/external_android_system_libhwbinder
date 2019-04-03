e91b269c639b654c347b90e6607b4b25a600b993
openvela-robot <openvela-robot@xiaomi.com>
2019-04-03 18:23:32 +0800

    Merge master@5406228 into git_qt-dev-plus-aosp. am: 5aabc5ebf6
    Change-Id: Id4cb9b80e5b237d2bf3b5e0b13af559519e9d5c6

--- IPCThreadState.cpp
+++ IPCThreadState.cpp
@@ -112,9 +112,8 @@ static const char *kCommandStrings[] = {
     "BC_DEAD_BINDER_DONE"
 };
 
-static const char* getReturnString(uint32_t cmd)
+static const char* getReturnString(size_t idx)
 {
-    size_t idx = cmd & 0xff;
     if (idx < sizeof(kReturnStrings) / sizeof(kReturnStrings[0]))
         return kReturnStrings[idx];
     else
