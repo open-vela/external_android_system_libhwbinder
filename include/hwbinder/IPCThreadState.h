ddf42f6377fbe6a31ac1de29d32553c682ee49ca
liujinye <liujinye@xiaomi.com>
2019-06-06 06:50:15 +0800

    Snap for 5636702 from 66b2b04145b1d1007f03356e0106b16ad5e8fa46 to qt-c2f2-release
    Change-Id: I1c4c88fd8d3babfcceab81086acf765d33186138

--- include/hwbinder/IPCThreadState.h
+++ include/hwbinder/IPCThreadState.h
@@ -89,6 +89,11 @@ public:
 
     static  void                shutdown();
 
+            // TODO(b/66905301): remove symbol
+private:
+    static  void                disableBackgroundScheduling(bool disable);
+public:
+
             // Call blocks until the number of executing binder threads is less than
             // the maximum number of binder threads threads allowed for this process.
             void                blockUntilThreadAvailable();
@@ -169,6 +174,7 @@ public:
             uid_t               mCallingUid;
             int32_t             mStrictModePolicy;
             int32_t             mLastTransactionBinderFlags;
+            sp<BHwBinder>         mContextObject;
             bool                mIsLooper;
             bool mIsPollingThread;
 
