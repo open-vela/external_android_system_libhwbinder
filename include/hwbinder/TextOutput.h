ba7a3a15cb734e722818b7c126686bdd364c5d4f
liujinye <liujinye@xiaomi.com>
2019-07-08 23:34:13 +0800

    Snap for 5622519 from aa22afd88b4ff0fc467fa57d88ebc87de3021319 to pi-platform-release
    Change-Id: I5949081369003cd14446767e848550e2197780c0

--- include/hwbinder/TextOutput.h
+++ include/hwbinder/TextOutput.h
@@ -39,7 +39,7 @@ public:
     
     class Bundle {
     public:
-        inline explicit Bundle(TextOutput& to) : mTO(to) { to.pushBundle(); }
+        inline Bundle(TextOutput& to) : mTO(to) { to.pushBundle(); }
         inline ~Bundle() { mTO.popBundle(); }
     private:
         TextOutput&     mTO;
@@ -81,7 +81,7 @@ TextOutput& operator<<(TextOutput& to, TextOutputManipFunc func);
 class TypeCode
 {
 public:
-    inline explicit TypeCode(uint32_t code);
+    inline TypeCode(uint32_t code);
     inline ~TypeCode();
 
     inline uint32_t typeCode() const;
