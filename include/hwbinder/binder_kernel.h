ac1d97f1eb78a565803260d37274d4bcac4b2044
liujinye <liujinye@xiaomi.com>
2020-01-22 10:41:03 +0800

    Fix addition/overflow checks.
    For unsigned arithmetic, use:
    
    (a + b < a) to detect whether a+b wraps
    (a > c / b) to detect whether a*b > c
    
    Bug: 120078455
    Test: builds and boots
    Change-Id: I738b90855b6b9a8effbf55a468c751b6d0b8edd9
    (cherry picked from commit d79ac373483c36f893981c7319d37a5ebbd63b98)

--- include/hwbinder/binder_kernel.h
+++ include/hwbinder/binder_kernel.h
@@ -17,15 +17,24 @@
 #ifndef ANDROID_HARDWARE_BINDER_KERNEL_H
 #define ANDROID_HARDWARE_BINDER_KERNEL_H
 
+#include <linux/android/binder.h>
+
 /**
- * Only need this file to fix the __packed__ keyword.
+ * This file exists because the uapi kernel headers in bionic are built
+ * from upstream kernel headers only, and not all of the hwbinder kernel changes
+ * have made it upstream yet. Therefore, the modifications to the
+ * binder header are added locally in this file.
  */
 
-// TODO(b/31559095): bionic on host
-#ifndef __ANDROID__
-#define __packed __attribute__((__packed__))
-#endif
+enum {
+	BINDER_BUFFER_FLAG_REF          = 1U << 1,
+};
 
-#include <linux/android/binder.h>
+enum {
+        FLAT_BINDER_FLAG_SCHEDPOLICY_MASK = 0x600,
+        FLAT_BINDER_FLAG_SCHEDPOLICY_SHIFT = 9,
+        FLAT_BINDER_FLAG_INHERIT_RT = 0x800,
+};
+
 
 #endif // ANDROID_HARDWARE_BINDER_KERNEL_H
