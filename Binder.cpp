94c9a9975518fc6766a519060ae18310cf7a83c2
openvela-robot <openvela-robot@xiaomi.com>
2021-07-12 11:08:30 +0800

    Merge RQ3A.210705.001 to aosp-master - DO NOT MERGE
    Merged-In: I6fc38d4dae7e8ed1eab1510eb6775c42344d0ce5
    Merged-In: I422190daf26845f8a9625dd798acb922b3673d68
    Merged-In: I422190daf26845f8a9625dd798acb922b3673d68
    Change-Id: I004a6beb4e89fe107be73ee4913e687dc44e5fa1

--- Binder.cpp
+++ Binder.cpp
@@ -16,20 +16,14 @@
 
 #include <hwbinder/Binder.h>
 
-#include <android-base/macros.h>
-#include <cutils/android_filesystem_config.h>
-#include <cutils/multiuser.h>
+#include <atomic>
+#include <utils/misc.h>
 #include <hwbinder/BpHwBinder.h>
 #include <hwbinder/IInterface.h>
-#include <hwbinder/IPCThreadState.h>
 #include <hwbinder/Parcel.h>
-#include <utils/Log.h>
-#include <utils/misc.h>
 
 #include <linux/sched.h>
 #include <stdio.h>
-
-#include <atomic>
 
 namespace android {
 namespace hardware {
@@ -118,15 +112,6 @@ status_t BHwBinder::transact(
 
     if (reply != nullptr && (flags & FLAG_CLEAR_BUF)) {
         reply->markSensitive();
-    }
-
-    // extra comment to try to force running all tests
-    if (UNLIKELY(code == HIDL_DEBUG_TRANSACTION)) {
-        uid_t uid = IPCThreadState::self()->getCallingUid();
-        if (multiuser_get_app_id(uid) >= AID_APP_START) {
-            ALOGE("Can not call IBase::debug from apps");
-            return PERMISSION_DENIED;
-        }
     }
 
     status_t err = NO_ERROR;
